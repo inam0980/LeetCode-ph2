d8793af439f726b389b6252a3a1e70b8d7f0b26e
Inam Khan <149032309+inam0980@users.noreply.github.com>
2026-05-26 23:39:22 +0530

    Time: 12 ms (57.40%) | Memory: 47.6 MB (10.79%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,69 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words.length == 0) {
+            return result;
+        }
+
+        HashMap<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int windowLen = wordLen * totalWords;
+
+        for (int i = 0; i < wordLen; i++) {
+
+            int left = i;
+            int count = 0;
+
+            HashMap<String, Integer> seen = new HashMap<>();
+
+            for (int right = i; right + wordLen <= s.length(); right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (wordCount.containsKey(word)) {
+
+                    seen.put(word, seen.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (seen.get(word) > wordCount.get(word)) {
+
+                        String leftWord = s.substring(left, left + wordLen);
+
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == totalWords) {
+
+                        result.add(left);
+
+                        String leftWord = s.substring(left, left + wordLen);
+
+                        seen.put(leftWord, seen.get(leftWord) - 1);
+
+                        left += wordLen;
+                        count--;
+                    }
+
+                } else {
+
+                    seen.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
